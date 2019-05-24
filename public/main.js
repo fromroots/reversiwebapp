abe03a89f1a8ec8ae2b476002f77faedfd481540
saurabhsharma <Fromroots@gmail.com>
2019-05-24 19:12:10 +0530

    final commit

--- public/main.js
+++ public/main.js
@@ -23,11 +23,59 @@ if('undefined' == typeof username || !username)
 }
 
 
-$('#messages').append('<h2>'+ username + '</h2>');
+//$('#messages').append('<h2>'+ username + '</h2>');
 
 
+var chat_room="one room";
 
-var socket=io.connect();
+
+/****connect to socket server */
+
+var socket = io.connect();
 socket.on('log',function(array){
    console.log.apply(console,array);
 });
+
+socket.on('join_room_response',function(payload){
+     if(payload.result == 'fail')
+     {
+         alert(payload.message);
+         return;
+     }
+     $('#messages').append('<p>New user joined the room:'+payload.username+'</p>');
+});
+
+function send_message()
+{
+    var payload= {};
+    payload.room=chat_room;
+    payload.username=username;
+    payload.message = $('#send_message_holder').val();
+    console.log('**** client log message:\'send_message\' \'payload:'+JSON.stringify(payload));
+   socket.emit('send_message',payload);
+
+}
+socket.on('send_message_response',function(payload){
+    if(payload.result == 'fail')
+    {
+        alert(payload.message);
+        return;
+    }
+    $('#messages1').append('<p><b>'+payload.username+'says:</b>'+payload.message+'</p>');
+});
+
+
+
+$(function(){
+    var payload= {};
+    payload.room=chat_room;
+    payload.username=username;
+
+    console.log('**** client log message: \'join_room\'payload:'+JSON.stringify(payload));
+  
+   socket.emit('join_room',payload);
+});
+
+
+
+
