2956fdcef8e27e0583b47e4e145f3941c78585cb
saurabhsharma <Fromroots@gmail.com>
2019-06-04 11:07:47 +0530

    Revert "Revert "june 3rd 2nd commit""
    This reverts commit bd62e6edd4826f07d8a313aba3b776ced08a778f.

--- public/main.js
+++ public/main.js
@@ -363,6 +363,7 @@ socket.on('game_update',function(payload)
         return;
     }
     $('#my_color').html('<h3 id="my_color">I am ' +my_color+'</h3>');
+    $('#my_color').append('<h4>It is '+payload.game.whose_turn+'\s turn</h4>');
 
     /* animate changes to the board */
     var blacksum=0;
